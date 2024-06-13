d67b3ef90be801c946d97d1b6d0c07800a03d51c
unknown <ikebelida156@gmail.com>
2024-06-14 07:34:07 +0800

    deploy najud

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -19,7 +19,6 @@ const MessageContainer = () => {
 				<NoChatSelected />
 			) : (
 				<>
-					{/* Header */}
 					<div className='bg-slate-500 px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
 						<span className='text-base-300 font-bold'>{selectedConversation.fullName}</span>
